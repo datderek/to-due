81933f78793b4e7c575070e9ab7697733b4944e4
Derek Tran <tran.derek.d.@gmail.com>
2024-02-27 17:21:57 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'main.html',
+      inject: 'body'
+    })
+  ]
+}
