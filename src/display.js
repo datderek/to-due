7d22810662e35024707b88067b3df58b1dee72cc
Derek Tran <tran.derek.d@gmail.com>
2024-02-29 13:47:08 -0800

    Add projects to dropdown

--- src/display.js
+++ src/display.js
@@ -1,25 +1,25 @@
 export default class Display {
   static content = document.querySelector("#content");
-  static nav = document.querySelector("#nav");
+  static nav = document.querySelector("#projects");
 
   static renderProjectTab(project) {
     const button = document.createElement("button");
     button.dataset.title = project.title;
     button.innerText = project.title;
+    button.classList.add("project");
     this.nav.appendChild(button);
   }
 
   static renderProject(project) {
     Display.clearContent();
-    const title = document.createElement("h2");
-    title.textContent = project.title;
+    const currentProject = document.getElementById("current-project");
+    currentProject.textContent = project.title;
     const list = document.createElement("ul");
     for (const task of project.todos) {
       const listItem = document.createElement("li");
       listItem.textContent = `${task.title}`;
       list.appendChild(listItem);
     }
-    this.content.appendChild(title);
     this.content.appendChild(list);
   }
 
